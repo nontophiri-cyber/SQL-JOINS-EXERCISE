5650246434f90755f735115759d9b51bace5b713
nontophiri-cyber <nontophiri@gmail.com>
2026-06-14 19:38:54 +0000

    Uploading querie

--- New Notebook 2026-06-13 22_21_32.sql
+++ New Notebook 2026-06-13 22_21_32.sql
@@ -3,13 +3,15 @@ SELECT*
 FROM workspace.default.users_table;
 
 SELECT*
-FROM workspace.default.subscriptions_table
+FROM workspace.default.subscriptions_table;
 ------------------------------------------------------
 
+ SELECT*
+    FROM workspace.default.users_table 
+    INNER JOIN workspace.default.subscriptions_table s
+    ON u.user_id = s.user_id;
 
-SELECT u.user_id, u.user_name, s.start_date, s.subscriptio_id
-FROM workspace.default.users_table u
-INNER JOIN workspace.default.subscriptions_table s
-ON u.user_id = s.user_id 
+
+
 
 
